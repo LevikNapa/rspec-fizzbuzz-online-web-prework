792873603d2b23a854e3c092d4f2d6b562468f9d
Levik Naparstek <levik010@gmail.com>
2019-02-05 21:57:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  fizzbuzz(3) = fizz
  if int % 3 == 0
  puts   "Fizz"
 elsif int % 5 == 0 
