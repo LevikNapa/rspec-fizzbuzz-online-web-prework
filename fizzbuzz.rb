60582009a5a5f84c5c57a67ff3b44f256a8a4c2b
Levik Naparstek <levik010@gmail.com>
2019-02-05 21:31:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
  puts   "Fizz"
-elseif int % 5 == 0 
+elsif int % 5 == 0 
  puts "Buzz" 
   
-elseif int % 5 && 3 == 0
+elsif int % 5 && 3 == 0
  puts  "FizzBuzz"
 
 else 
