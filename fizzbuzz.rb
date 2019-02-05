b0845d7875e081e4dcfb478a0397e757f1fc4966
Levik Naparstek <levik010@gmail.com>
2019-02-05 21:58:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  fizzbuzz(3) = Fizz
  if int % 3 == 0
  puts   "Fizz"
 elsif int % 5 == 0 
